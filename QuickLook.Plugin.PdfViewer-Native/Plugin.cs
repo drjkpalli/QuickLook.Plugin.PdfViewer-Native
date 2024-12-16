6c399751367bcc30435721772f6c9c1ceb3b2474
ema <mccoy39082@163.com>
2024-12-16 10:18:36 +0800

    First commit

---
+++ QuickLook.Plugin.PdfViewer-Native/Plugin.cs
@@ -0,0 +1,64 @@
+﻿// Copyright © 2024 ema
+//
+// This file is part of QuickLook program.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using QuickLook.Common.Plugin;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace QuickLook.Plugin.PDFViewerNative;
+
+public class Plugin : IViewer
+{
+    public int Priority => 1;
+
+    private WebpagePanel _panel;
+
+    public void Init()
+    {
+    }
+
+    public bool CanHandle(string path)
+    {
+        if (File.Exists(path) && Path.GetExtension(path).Equals(".pdf", StringComparison.OrdinalIgnoreCase))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void Prepare(string path, ContextObject context)
+    {
+        context.PreferredSize = new Size(1280, 720);
+    }
+
+    public void View(string path, ContextObject context)
+    {
+        _panel = new WebpagePanel();
+        context.ViewerContent = _panel;
+        context.Title = Path.GetFileName(path);
+
+        _panel.NavigateToFile(path);
+        _panel.Dispatcher.Invoke(() => { context.IsBusy = false; }, DispatcherPriority.Loaded);
+    }
+
+    public void Cleanup()
+    {
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ QuickLook.Plugin.PdfViewer-Native/WebpagePanel.cs
@@ -0,0 +1,115 @@
+// Copyright © 2021 Paddy Xu and Frank Becker
+//
+// This file is part of QuickLook program.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.Wpf;
+using QuickLook.Common.Helpers;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace QuickLook.Plugin.PDFViewerNative;
+
+public class WebpagePanel : UserControl
+{
+    private Uri _currentUri;
+    private WebView2 _webView;
+
+    public WebpagePanel()
+    {
+        if (!Helper.IsWebView2Available())
+        {
+            Content = CreateDownloadButton();
+        }
+        else
+        {
+            _webView = new WebView2
+            {
+                CreationProperties = new CoreWebView2CreationProperties
+                {
+                    UserDataFolder = Path.Combine(SettingHelper.LocalDataPath, @"WebView2_Data\\"),
+                },
+                DefaultBackgroundColor = OSThemeHelper.AppsUseDarkTheme() ? Color.FromArgb(255, 32, 32, 32) : Color.White, // Prevent white flash in dark mode
+            };
+            _webView.NavigationStarting += NavigationStarting_CancelNavigation;
+            _webView.NavigationCompleted += WebView_NavigationCompleted;
+            Content = _webView;
+        }
+    }
+
+    public void NavigateToFile(string path)
+    {
+        var uri = Path.IsPathRooted(path) ? Helper.FilePathToFileUrl(path) : new Uri(path);
+
+        NavigateToUri(uri);
+    }
+
+    public void NavigateToUri(Uri uri)
+    {
+        if (_webView == null)
+            return;
+
+        _webView.Source = uri;
+        _currentUri = _webView.Source;
+    }
+
+    public void NavigateToHtml(string html)
+    {
+        _webView?.EnsureCoreWebView2Async()
+            .ContinueWith(_ => Dispatcher.Invoke(() => _webView?.NavigateToString(html)));
+    }
+
+    private void NavigationStarting_CancelNavigation(object sender, CoreWebView2NavigationStartingEventArgs e)
+    {
+        if (e.Uri.StartsWith("data:")) // when using NavigateToString
+            return;
+
+        var newUri = new Uri(e.Uri);
+        if (newUri == _currentUri) return;
+        e.Cancel = true;
+    }
+
+    private void WebView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+    {
+        _webView.DefaultBackgroundColor = Color.White; // Reset to white after page load to match expected default behavior
+    }
+
+    public void Dispose()
+    {
+        _webView?.Dispose();
+        _webView = null;
+    }
+
+    private object CreateDownloadButton()
+    {
+        var button = new Button
+        {
+            Content = TranslationHelper.Get("WEBVIEW2_NOT_AVAILABLE",
+                domain: Assembly.GetExecutingAssembly().GetName().Name),
+            HorizontalAlignment = HorizontalAlignment.Center,
+            VerticalAlignment = VerticalAlignment.Center,
+            Padding = new Thickness(20, 6, 20, 6)
+        };
+        button.Click += (sender, e) => Process.Start("https://go.microsoft.com/fwlink/p/?LinkId=2124703");
+
+        return button;
+    }
+}
